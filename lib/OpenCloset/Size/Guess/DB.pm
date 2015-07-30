9c005629376d487473431bdae6b4c0abd5cb74b5
Keedi Kim <keedi.k@gmail.com>
2015-07-31 02:36:47 +0900

    Ready to dzil

---
+++ lib/OpenCloset/Size/Guess/DB.pm
@@ -0,0 +1,25 @@
+package OpenCloset::Size::Guess::DB;
+# ABSTRACT: OpenCloset::Size::Guess driver for the database
+
+use utf8;
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+=for Pod::Coverage BUILDARGS
+
+=head1 SYNOPSIS
+
+    ...
+
+
+=head1 DESCRIPTION
+
+...
